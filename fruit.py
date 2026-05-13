d2eba15753c74afdef3f3e2d917e93bf1fd88378
ranahaider3636-oss <ranahaider3636@gmail.com>
2026-05-13 16:07:36 +0500

    liked fruits

---
+++ fruit.py
@@ -0,0 +1,12 @@
+orange=1
+apple=2
+banana=3
+a=int(input("Enter a number from 1 to 3: "))
+if a==orange:
+    print("I like orange.")
+elif a==apple:
+    print("I like apple.")
+elif a==banana:
+    print("I like banana.")
+else:
+    print("Invalid selection. Please enter a number from 1 to 3.")
