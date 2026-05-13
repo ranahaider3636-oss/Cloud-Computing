7aa43e9f70f85d69558526652a5979a4a5bf6a9a
ranahaider3636-oss <ranahaider3636@gmail.com>
2026-05-13 15:56:58 +0500

    a is greater

---
+++ comp.py
@@ -0,0 +1,8 @@
+a=int(input("Enter First number: "))
+b=int(input("Enter Second number: "))
+if a>b:
+    print("First number is greater than the second number.")
+elif b>a:
+    print("Second number is greater than the first number.")
+else:
+    print("Both numbers are equal.")
